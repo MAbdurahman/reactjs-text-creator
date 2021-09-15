ae4ca8dddeb5b46b9d16611e7ef36a1b509fdba7
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-15 16:58:07 -0500

    Add two new state variables using state hooks

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './../data'
 
 
 function App() {
-  return (
-    <div>
-      <h1>This is App</h1>
-    </div>
-  );
+	/**************** variables ****************/
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+	/**************** functions ****************/
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+	return (
+		<section className='section-center'>
+			<h3>tired of boring lorem ipsum?</h3>
+			<form className='lorem-form' onSubmit={handleSubmit}>
+				<label htmlFor='amount'>paragraphs:</label>
+				<input
+					type='number'
+					name='amount'
+					id='amount'
+					value={count}
+					onChange={e => setCount(e.target.value)}
+				/>
+				<button className='btn'>generate</button>
+			</form>
+			<article className='lorem-text'>
+				{text.map((item, index) => {
+					return <p key={index}>{item}</p>;
+				})}
+			</article>
+		</section>
+	);
 }
 
 export default App;
