c4edd74b080dcb3d101323c3bb8e7045493f7a21
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-15 17:13:51 -0500

    Add functionality to handleSubmit method

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
-import data from './../data'
-
+import data from './../data';
 
 function App() {
 	/**************** variables ****************/
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState([]);
+	const [count, setCount] = useState(0);
+	const [text, setText] = useState([]);
 	/**************** functions ****************/
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
+	const handleSubmit = e => {
+		e.preventDefault();
+		let amount = parseInt(count);
+		if (count <= 0) {
+			amount = 1;
+		}
+		if (count > 8) {
+			amount = 8;
+		}
+		setText(data.slice(0, amount));
+	};
 	return (
 		<section className='section-center'>
 			<h3>tired of boring lorem ipsum?</h3>
